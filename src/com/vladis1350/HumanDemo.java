1bdbb530fa941578ebd5379eeccd599c6c52be13
admin <zerdonik777@gmail.com>
2020-02-12 16:25:08 +0300

    Home Work about human

---
+++ src/com/vladis1350/HumanDemo.java
@@ -0,0 +1,11 @@
+package com.vladis1350;
+
+import com.vladis1350.bean.Human;
+
+public class HumanDemo {
+    public static void main(String[] args) {
+        Human human = new Human("Vladislav", 24);
+
+        human.greet();
+    }
+}
---
+++ src/com/vladis1350/bean/Human.java
@@ -0,0 +1,35 @@
+package com.vladis1350.bean;
+
+public class Human {
+    private String name;
+    private int age;
+
+    public Human(){
+
+    }
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void greet() {
+        System.out.println("Hi! My name is " + getName() + ", I'm " + getAge() + " years old");
+    }
+}
